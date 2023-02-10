c97f2f9fc51836c665cb28ba04345698cdb087ae
MateuszGontarek <mateuszgontarek2020@gmail.com>
2023-02-10 22:41:28 +0100

    10/02/2023 v2

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -14,3 +14,41 @@ def test_app_user_create_endpoint() -> None:
     client = app.test_client()
     response = client.post(path='/users', json=payload)
     assert response.status_code == 200
+
+
+def test_app_user_update_endpoint() -> None:
+    payload = {"first_name": "Jan", "last_name": "Kowalski"}
+    client = app.test_client()
+    response = client.put(path='/users', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_get_endpoint() -> None:
+    payload = {"first_name": "Jan", "last_name": "Kowalski"}
+    client = app.test_client()
+    response = client.get(path='/users', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_delete_endpoint() -> None:
+    payload = {"first_name": "Jan", "last_name": "Kowalski"}
+    client = app.test_client()
+    response = client.delete(path='/users', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_get_endpoint() -> None:
+    payload = {"first_name": "Jan", "last_name": "Kowalski"}
+    client = app.test_client()
+    response = client.get(path='/users', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_delete_endpoint() -> None:
+    payload = {"first_name": "Jan", "last_name": "Kowalski"}
+    client = app.test_client()
+    response = client.delete(path='/users', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_patch_endpoint() -> None:
+    payload = {"first_name": "Jan", "last_name": "Kowalski"}
+    client = app.test_client()
+    response = client.patch(path='/users', json=payload)
+    assert response.status_code == 200
+    
